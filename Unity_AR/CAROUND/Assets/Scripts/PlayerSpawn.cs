0990b4b08388e36c4e12d0cb5746e6272338253f
kindcode <kindcode@gmail.com>
2019-10-12 15:39:26 +0900

    1
    1

---
+++ Unity_AR/CAROUND/Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    private GameObject Player; 
+    public Transform _PlayerSpawn;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Player = Resources.Load("Classic_16") as GameObject;
+        _PlayerSpawn = GetComponent<Transform>();
+        ViewCar();
+    }
+    public void ViewCar()
+    {
+        GameObject.Instantiate(Player, _PlayerSpawn.transform.position, _PlayerSpawn.transform.rotation);
+    }
+}
